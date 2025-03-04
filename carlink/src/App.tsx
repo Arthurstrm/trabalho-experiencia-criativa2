cc47450c989291ffdbe0a726a0bf02fc69b40fdd
Fabio-Spiller <devfabiospiller@gmail.com>
2025-03-04 19:55:49 -0300

    teste do test

--- carlink/src/App.tsx
+++ carlink/src/App.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from 'components/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <body>
-          <h1>teste</h1>
-        </body>
-      </header>
-    </div>
+    <Navbar></Navbar>
   );
 }
 
