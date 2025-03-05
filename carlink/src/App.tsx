1889b4ee53f7ec6e1e6f7d3c1cc8692414364dcb
Foren4772 <vini.gogolla@outlook.com>
2025-03-05 20:47:02 -0300

    criação Cadastro funcionario

--- carlink/src/App.tsx
+++ carlink/src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import CadastroUsuario from './pages/CadastroUsuario';
+import CadastroFuncionario from './pages/CadastroFuncionario';
 
 function App() {
   return (
     <div className="container mt-5">
       <div className="row justify-content-center">
         <div className="col-md-6 col-lg-4">
-          <h1>Cadastro de Usuário</h1>
-          <CadastroUsuario />
+          <CadastroFuncionario/>
         </div>
       </div>
     </div>
