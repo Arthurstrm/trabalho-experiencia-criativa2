cb6a48fbbbb981f8ce75fe45e1ac1b6cb7ac5764
Lucas Gabriel Nunes dos Santos <lg03066385@gmail.com>
2025-03-05 21:01:50 -0300

    cadadastro de carros
    teste

---
+++ carlink/src/pages/CadastroCarros.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+function CadastroCarro() {
+    return (
+        <form>
+            <div className="mb-3">
+                <label htmlFor="marca" className="form-label">Marca</label>
+                <input type="text" className="form-control" id="marca" />
+            </div>
+
+            <div className="mb-3">
+                <label htmlFor="modelo" className="form-label">Modelo</label>
+                <input type="text" className="form-control" id="modelo" />
+            </div>
+
+            <div className="mb-3">
+                <label htmlFor="ano" className="form-label">Ano</label>
+                <input type="number" className="form-control" id="ano" />
+            </div>
+
+            <div className="mb-3">
+                <label htmlFor="placa" className="form-label">Placa</label>
+                <input type="text" className="form-control" id="placa" />
+            </div>
+
+            <div className="mb-3">
+                <label htmlFor="renavam" className="form-label">RENAVAM</label>
+                <input type="text" className="form-control" id="renavam" />
+            </div>
+
+            <div className="mb-3">
+                <label htmlFor="chassi" className="form-label">Chassi</label>
+                <input type="text" className="form-control" id="chassi" />
+            </div>
+
+            <div className="mb-3 form-check">
+                <input type="checkbox" className="form-check-input" id="veiculoFinanciado" />
+                <label className="form-check-label" htmlFor="veiculoFinanciado">Veículo financiado</label>
+            </div>
+
+            <button type="submit" className="btn btn-primary">Cadastrar</button>
+        </form>
+    );
+}
+
+export default CadastroCarro;
