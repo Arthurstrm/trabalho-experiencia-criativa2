ec82f37d9390e8bb7ceda4a5dc7498ab4190212f
Fabio-Spiller <devfabiospiller@gmail.com>
2025-03-05 21:29:25 -0300

    CadastroUsuario

--- carlink/src/App.tsx
+++ carlink/src/App.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import CadastroUsuario from './pages/CadastroUsuario';
-import CadastroFuncionario from './pages/CadastroFuncionario';
 
 function App() {
   return (
-    <div className="container mt-5">
-      <div className="row justify-content-center">
-        <div className="col-md-6 col-lg-4">
-          <CadastroFuncionario/>
-        </div>
-      </div>
-    </div>
+    
+          <CadastroUsuario />
+      
   );
 }
 
--- carlink/src/pages/CadastroUsuario.js
+++ carlink/src/pages/CadastroUsuario.js
@@ -2,45 +2,55 @@ import React from 'react';
 
 function CadastroUsuario() {
   return (
-    
-    <form>
-      {/* Cadastro Email Usuario */}
-      <div className="mb-3">
-        <label htmlFor="exampleInputEmail1" className="form-label" >Email</label>
-        <input
-          type="email"
-          className="form-control"
-          id="inputEmailCadastroUsuario"
-          placeholder="exemplo@email.com"
-        />
-      </div>
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6 col-lg-4">
+          
+          <h1>Cadastro Usuário</h1>
+          
+          <form>
+            {/* Cadastro Email Usuario */}
+            <div className="mb-3">
+              <label htmlFor="inputEmailCadastroUsuario" className="form-label">Email</label>
+              <input
+                type="email"
+                className="form-control"
+                id="inputEmailCadastroUsuario"
+                placeholder="exemplo@email.com"
+              />
+            </div>
 
-      {/* Cadastro email Usuario */}
-      <div className="mb-3">
-        <label htmlFor="inputPasswordCadastroUsuario" className="form-label">Password</label>
-        <input
-          type="password"
-          className="form-control"
-          id="inputPasswordCadastroUsuario"
-          aria-describedby="passwordHelp"
-          placeholder="********"
-        />
-        <div id="passwordHelp" className="form-text">Senha de 8 a 16 dígitos.</div>
+            {/* Cadastro Senha Usuario */}
+            <div className="mb-3">
+              <label htmlFor="inputPasswordCadastroUsuario" className="form-label">Senha</label>
+              <input
+                type="password"
+                className="form-control"
+                id="inputPasswordCadastroUsuario"
+                aria-describedby="passwordHelp"
+                placeholder="********"
+              />
+              <div id="passwordHelp" className="form-text">Senha de 8 a 16 dígitos.</div>
+            </div>
+
+            {/* Checkbox */}
+            <div className="mb-3 form-check">
+              <input
+                type="checkbox"
+                className="form-check-input"
+                id="exampleCheck1"
+              />
+              <label className="form-check-label" htmlFor="exampleCheck1">
+                Concordo com os <a href="#">Termos de uso.</a>
+              </label>
+            </div>
+
+            {/* Botão */}
+            <button type="submit" className="btn btn-primary">Cadastrar</button>
+          </form>
+        </div>
       </div>
-     
-     {/* Checkbox */}
-      <div className="mb-3 form-check">
-        <input
-          type="checkbox"
-          className="form-check-input"
-          id="exampleCheck1"
-        />
-        <label className="form-check-label" htmlFor="exampleCheck1">Concordo com os <a href="#">Termos de uso.</a></label>
-      </div>
-      
-      {/* Botão */}
-      <button type="submit" className="btn btn-primary">Submit</button>
-    </form>
+    </div>
   );
 }
 
