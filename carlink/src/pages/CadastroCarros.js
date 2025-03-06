9d00febc8f0c794227f601f4ebe1daa7d52e69e5
Lucas Gabriel Nunes dos Santos <lg03066385@gmail.com>
2025-03-05 23:01:16 -0300

    centralizado

--- carlink/src/pages/CadastroCarros.js
+++ carlink/src/pages/CadastroCarros.js
@@ -2,41 +2,45 @@ import React from 'react';
 
 function CadastroCarro() {
     return (
-        <form>
-            <div className="mb-3">
-                <label htmlFor="marca" className="form-label">Marca</label>
-                <input type="text" className="form-control" id="marca" />
+        <div className="container mt-5">
+            <div className="row justify-content-center">
+                <div className="col-md-6 col-lg-4">
+                    <form>
+                        <div className="mb-3">
+                            <label htmlFor="marca" className="form-label">Marca</label>
+                            <input type="text" className="form-control" id="marca" />
+                        </div>
+
+                        <div className="mb-3">
+                            <label htmlFor="modelo" className="form-label">Modelo</label>
+                            <input type="text" className="form-control" id="modelo" />
+                        </div>
+
+                        <div className="mb-3">
+                            <label htmlFor="ano" className="form-label">Ano</label>
+                            <input type="number" className="form-control" id="ano" />
+                        </div>
+
+                        <div className="mb-3">
+                            <label htmlFor="placa" className="form-label">Placa</label>
+                            <input type="text" className="form-control" id="placa" />
+                        </div>
+
+                        <div className="mb-3">
+                            <label htmlFor="renavam" className="form-label">RENAVAM</label>
+                            <input type="text" className="form-control" id="renavam" />
+                        </div>
+
+                        <div className="mb-3">
+                            <label htmlFor="chassi" className="form-label">Chassi</label>
+                            <input type="text" className="form-control" id="chassi" />
+                        </div>
+
+                        <button type="submit" className="btn btn-primary">Cadastrar</button>
+                    </form>
+                </div>
             </div>
-
-            <div className="mb-3">
-                <label htmlFor="modelo" className="form-label">Modelo</label>
-                <input type="text" className="form-control" id="modelo" />
-            </div>
-
-            <div className="mb-3">
-                <label htmlFor="ano" className="form-label">Ano</label>
-                <input type="number" className="form-control" id="ano" />
-            </div>
-
-            <div className="mb-3">
-                <label htmlFor="placa" className="form-label">Placa</label>
-                <input type="text" className="form-control" id="placa" />
-            </div>
-
-            <div className="mb-3">
-                <label htmlFor="renavam" className="form-label">RENAVAM</label>
-                <input type="text" className="form-control" id="renavam" />
-            </div>
-
-            <div className="mb-3">
-                <label htmlFor="chassi" className="form-label">Chassi</label>
-                <input type="text" className="form-control" id="chassi" />
-            </div>
-
-
-
-            <button type="submit" className="btn btn-primary">Cadastrar</button>
-        </form>
+        </div>
     );
 }
 
