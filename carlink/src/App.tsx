91eb8a7a7d6035ba0f143d667eb64b08721959d1
Fabio-Spiller <devfabiospiller@gmail.com>
2025-03-05 21:54:43 -0300

    Navbar

--- carlink/src/App.tsx
+++ carlink/src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import CadastroUsuario from './pages/CadastroUsuario';
+import CadastroCarros from './pages/CadastroCarros';
+import CadastroFuncionario from './pages/CadastroFuncionario';
+// import Login from './pages/Login';
 
 function App() {
   return (
-    
-          <CadastroUsuario />
-      
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/cadastro-usuario" element={<CadastroUsuario />} />
+        <Route path="/cadastro-carros" element={<CadastroCarros />} />
+        <Route path="/cadastro-funcionario" element={<CadastroFuncionario />} />
+        {/* <Route path="/login" element={<Login />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
--- carlink/src/components/Navbar.js
+++ carlink/src/components/Navbar.js
@@ -1,31 +1,51 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          Minha Aplicação
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/cadastro-usuario">
+                Cadastro de Usuário
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/cadastro-carros">
+                Cadastro de Carros
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/cadastro-funcionario">
+                Cadastro de Funcionário
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/login">
+                Login
+              </Link>
+            </li>
           </ul>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
