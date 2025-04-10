a0cb08789cca41a69d66882da38503dc36e94cbe
Fabio-Spiller <devfabiospiller@gmail.com>
2025-04-10 05:21:33 -0300

    CadastroLogin

--- carlink/src/App.tsx
+++ carlink/src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import CadastroUsuario from './pages/CadastroUsuario';
 import CadastroCarros from './pages/CadastroCarros';
 import CadastroFuncionario from './pages/CadastroFuncionario';
 import Login from './pages/Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './custom.scss';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import CadastroLogin from './pages/CadastroLogin';
 
 function App() {
   return (
@@ -16,7 +15,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/cadastro-usuario" element={<CadastroUsuario />} />
+        <Route path="/cadastro-login" element={<CadastroLogin />} />
         <Route path="/cadastro-carros" element={<CadastroCarros />} />
         <Route path="/cadastro-funcionario" element={<CadastroFuncionario />} />
         <Route path="/login" element={<Login />} />
--- carlink/src/components/Navbar.js
+++ carlink/src/components/Navbar.js
@@ -23,8 +23,8 @@ function Navbar() {
         <div className="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <Link className="nav-link" to="/cadastro-usuario">
-                Cadastro de Usuário
+              <Link className="nav-link" to="/cadastro-login">
+                Cadastro de Login
               </Link>
             </li>
             <li className="nav-item">
